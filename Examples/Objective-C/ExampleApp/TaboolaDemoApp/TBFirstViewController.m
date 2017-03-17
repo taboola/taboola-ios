a157694819b989a4113d4bb13e5d418d4b8c8707
Vasyl Human <pesukk24@gmail.com>
2017-03-17 14:38:18 +0200

    - changed samples;

--- Examples/Objective-C/ExampleApp/TaboolaDemoApp/TBFirstViewController.m
+++ Examples/Objective-C/ExampleApp/TaboolaDemoApp/TBFirstViewController.m
@@ -18,7 +18,7 @@
 	mTaboolaView.delegate = self;
     mTaboolaView.ownerViewController = self;
     mTaboolaView.mode = @"thumbnails-sdk1";
-	mTaboolaView.publisher = @"betterbytheminute";
+	mTaboolaView.publisher = @"betterbytheminute-app";
     mTaboolaView.pageType = @"article";
     mTaboolaView.pageUrl = @"http://www.example.com";
     mTaboolaView.placement = @"Mobile";
@@ -35,20 +35,6 @@
     [mTaboolaView fetchContent];
 }
 
-- (void)viewWillAppear:(BOOL)animated{
-	[super viewWillAppear:animated];
-
-	//show page content
-	NSMutableAttributedString * lText = [[NSMutableAttributedString alloc] initWithString:@"Pellentesque habitant morbi tristique \n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla hendrerit magna, eget dignissim tortor condimentum in. Nulla ac est eu nisl ultrices sagittis non quis dui. Nam laoreet tortor id turpis consectetur, vel tristique diam lacinia. Etiam euismod velit eu tortor scelerisque tempus. Morbi viverra tortor eu hendrerit vestibulum. Donec mauris mauris, lobortis ac consequat et, venenatis sit amet arcu. Sed ut sapien ornare, iaculis mi quis, rutrum lorem. Aliquam vel adipiscing nunc. Etiam elementum vitae ante et luctus. Proin tempor arcu urna, vitae dapibus diam tempus vitae. Sed lacinia dignissim neque, a faucibus lacus malesuada quis. Cras tortor risus, egestas vel metus a, pharetra dapibus nunc. \n\nInterdum et malesuada fames ac ante ipsum primis in faucibus. Ut non venenatis ipsum, sit amet vestibulum sem. Curabitur sit amet quam rutrum, condimentum turpis sit amet, pharetra magna. Nullam sodales lectus ut semper imperdiet. Donec vehicula justo non mauris ullamcorper, eu sodales purus tempus. Praesent pharetra felis nec neque volutpat pellentesque. Duis mollis urna dui, a malesuada orci laoreet eu. Fusce diam nisl, consequat adipiscing mi viverra, eleifend tempus est. In risus risus, imperdiet ut metus eu, malesuada tincidunt mi."];
-    
-	[lText addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"Arial-BoldMT" size:22] range:NSMakeRange(0, HEADER_LENGTH)];
-	[mTextLabel setAttributedText:lText];
-	[mTextLabel sizeToFit];
-	
-    [mScrollView setContentSize:CGSizeMake(mScrollView.frame.size.width, mTextLabel.frame.size.height + mTaboolaView.frame.size.height + BOTTOM_MARGIN)];
-    [mTaboolaView setCenter:CGPointMake(mScrollView.frame.size.width/2, mScrollView.contentSize.height - mTaboolaView.frame.size.height/2)];
-}
-
 #pragma mark - Rotation
 - (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation{
 	[super didRotateFromInterfaceOrientation:fromInterfaceOrientation];
@@ -62,11 +48,6 @@
         NSLog(@"organic items should open as native app pages.");
     }
 	return YES;
-}
-
-- (void)taboolaViewResizeHandler{
-    [mScrollView setContentSize:CGSizeMake(mScrollView.frame.size.width, mTextLabel.frame.size.height + mTaboolaView.frame.size.height + BOTTOM_MARGIN)];
-    [mTaboolaView setCenter:CGPointMake(mScrollView.frame.size.width/2, CGRectGetMaxY(mTextLabel.frame) + mTaboolaView.frame.size.height/2)];
 }
 
 - (void)taboolaDidReceiveAd:(UIView *)view{
